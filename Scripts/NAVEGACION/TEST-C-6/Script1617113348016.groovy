9ae5737c51e9a151e4c92556504be0bebd986f1a
joel_ <joel_@DESKTOP-EAUO6T0>
2021-03-30 16:42:49 -0400

    COMENTARIO PARA LE FOR-LOOP COMENTARIO PARA PODER IDENTIFICAR EL LFOR LOOP Y SU FUNCION.

--- Scripts/NAVEGACION/TEST-C-6/Script1617113348016.groovy
+++ Scripts/NAVEGACION/TEST-C-6/Script1617113348016.groovy
@@ -16,13 +16,9 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-
-
+import java.lang.StringBuilder as StringBuilder
 
 //HACER UNA BUSQUEDA DE UN ARTICULO -----------TEST-C-6------------
-
-
-
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.amazon.com/-/es/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
@@ -39,21 +35,21 @@ WebUI.setText(findTestObject('LOGIN_OR/Page_Amazon Sign-In/input_Forgot your pas
 
 WebUI.click(findTestObject('LOGIN_OR/Page_Amazon Sign-In/input_Enter your password_signInSubmit'))
 
-for (def rowdata = 1; rowdata <= findTestData("Data Files/BUSCADOR/BUSCADOR_DATA").getRowNumbers(); rowdata++) {
-	
+// ESTE FOR LOOP LO QUE HACE ES ITERAR DESDE LA TABLA "BUSCADOR_DATA" Y HACER UNA BUSQUEDA POR CADA UNO DE LOS DATOS QUE TENEMOS EN ESA TABLA.
+for (def rowdata = 1; rowdata <= findTestData('Data Files/BUSCADOR/BUSCADOR_DATA').getRowNumbers(); rowdata++) {
     WebUI.setText(findTestObject('ELEMENT_TO_VERIFY_OR/Page_Gold Box Deals  Todays Deals - Amazon.com/Page_Amazon.com Online Shopping for Electronics, Apparel, Computers, Books, DVDs  more/input_All_field-keywords'), 
-         findTestData("Data Files/BUSCADOR/BUSCADOR_DATA").getValue(1, rowdata))
+        findTestData('Data Files/BUSCADOR/BUSCADOR_DATA').getValue(1, rowdata))
 
     WebUI.click(findTestObject('Object Repository/ELEMENT_TO_VERIFY_OR/Page_Amazon.com Online Shopping for Electronics, Apparel, Computers, Books, DVDs  more/input_All_nav-search-submit-button'))
 
-	//WebUI.delay(2)
-	
-	WebUI.back()
-	
-	//WebUI.delay(1)
+    //WebUI.delay(2)
+    WebUI.back()
+
+    //WebUI.delay(1)
     Thread.sleep(300)
+
+   
 }
 
 WebUI.delay(3)
-
 
